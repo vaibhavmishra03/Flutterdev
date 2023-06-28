badda4611166d6f65657b6b110b90d15b773bc45
Vaibhav Mishra <vaibhavmishra@Vaibhavs-MacBook-Air.local>
2023-06-28 16:55:09 +0530

    day 11 done context,constraints was learned

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -5,18 +5,18 @@ class HomePage extends StatelessWidget {
   const HomePage({super.key});
   final int days = 30;
   final String name = "Vaibhav";
-
+//day learned about context,constraints
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text(
           "CATALOGUE APP",
-          style: TextStyle(fontStyle: FontStyle.italic,color: Colors.black),
+          style: TextStyle(fontStyle: FontStyle.italic, color: Colors.black),
         ),
       ),
-      body: const Center(
-        child: Text("FLUTTER HOME"),
+      body: Center(
+        child: Text("FLUTTER HOME "),
       ),
       drawer: const Mydrawer(),
     );
