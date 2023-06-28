7121496d62782f37b55af2de85a1830972594919
Vaibhav Mishra <vaibhavmishra@Vaibhavs-MacBook-Air.local>
2023-06-28 11:42:16 +0530

    catalog models details updated

---
+++ lib/models/catalog.dart
@@ -0,0 +1,20 @@
+class Item {
+  final String id;
+  final String name;
+  final String desc;
+  final num price;
+  final String color;
+  final String imageurl;
+
+  Item({this.id="", this.name="", this.desc="", this.price=0, this.color="", this.imageurl=""});
+}
+final product=[
+  Item( id: "Vaibhav001",
+name: "iphone 14 pro max",
+desc: "Phone for th future",
+price: 999,
+color: "#33505a",
+imageurl: "https://m.media-amazon.com/images/I/71yzJoE7WlL._AC_UF1000,1000_QL80_.jpg",
+
+  )
+];
