2e1bf118c15bf25ab834bc5ac54eab971c4843ed
Vaibhav Mishra <vaibhavmishra@Vaibhavs-MacBook-Air.local>
2023-06-30 08:42:21 +0530

    day 14 done learned about gridview

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -22,19 +22,19 @@ class _HomePageState extends State<HomePage> {
   }
 
   loadData() async {
-   //await Future.delayed(Duration(seconds: 2));
+    //await Future.delayed(Duration(seconds: 2));
     final catalogJson =
         await rootBundle.loadString("assets/files/catalog.json");
- //await Future.delayed(Duration(seconds: 2));
+    await Future.delayed(Duration(seconds: 2));
     //print(catalogJson);
     final decodeData = jsonDecode(catalogJson);
     //print(decodeData);
     var productsData = decodeData["products"];
-     Catalogue.items = List.from(productsData).map<Item>((item) => Item.fromMap(item)).toList();
+    Catalogue.items = List.from(productsData)
+        .map<Item>((item) => Item.fromMap(item))
+        .toList();
     //print(productsData);
-    setState(() {
-      
-    });
+    setState(() {});
   }
 
   @override
@@ -47,16 +47,40 @@ class _HomePageState extends State<HomePage> {
           style: TextStyle(fontStyle: FontStyle.italic, color: Colors.black),
         ),
       ),
-      body: (Catalogue.items.isNotEmpty ) ? ListView.builder(
-        itemCount: Catalogue.items.length,
-        itemBuilder: (context, index) {
-          return Itemwidget(
-            item: Catalogue.items[index],
-          );
-        },
-      ) : Center(
-        child: const CircularProgressIndicator(),
-         ),
+      body: (Catalogue.items.isNotEmpty)
+          ? GridView.builder(
+              gridDelegate:
+                  SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+              itemBuilder: (context, index) {
+                final item = Catalogue.items[index];
+                return Padding(
+                  padding: const EdgeInsets.all(4.0),
+                  child: Card(
+                      clipBehavior: Clip.antiAlias,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10.0)),
+                      child: GridTile(
+                        header: Container(
+                          child: Text(
+                            item.name,
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          padding: EdgeInsets.all(12.0),
+                          decoration: BoxDecoration(
+                            color: Colors.deepPurple,
+                          ),
+                        ),
+                        footer: Text("\$${item.price.toString()}",
+                            style: TextStyle(fontWeight: FontWeight.bold)),
+                        child: Image.network(item.imageurl, fit: BoxFit.cover),
+                      )),
+                );
+              },
+              itemCount: Catalogue.items.length,
+            )
+          : Center(
+              child: const CircularProgressIndicator(),
+            ),
       drawer: const Mydrawer(),
     );
   }
